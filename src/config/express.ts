319e0618125d4e3a3f00826a04089f681e035960
Answer-kino <kino@ianswer.co.kr>
2022-04-12 14:11:00 +0900

    feat: use apiRouter

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import indexRouter from "../route/index";
+import apiRouter from "../route/api";
 import { notFoundErrorHandler } from "../middleware/apiErrorHandler";
 
 const app = express();
 
 app.use("/", indexRouter);
+app.use("/api", apiRouter);
 
 app.use(notFoundErrorHandler);
 export default app;
