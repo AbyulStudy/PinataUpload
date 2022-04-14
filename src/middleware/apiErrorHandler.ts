d3596f94c522e84c5cc107fc7b95b714f37612fe
devbyul <kino@ianswer.co.kr>
2022-04-14 10:10:08 +0900

    feat: apiErrprHandler

--- src/middleware/apiErrorHandler.ts
+++ src/middleware/apiErrorHandler.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
 import HttpStatusCodes from "http-status-codes";
+import ApiError from "../utility/apiError";
 
 /* NOT FOUND(404) middleware to catch error response */
 export const notFoundErrorHandler = (req: Request, res: Response) => {
   res.status(HttpStatusCodes.NOT_FOUND).end();
+};
+
+export const errorHandler = (err: ApiError, req: Request, res: Response) => {
+  res.status(err.status || HttpStatusCodes.INTERNAL_SERVER_ERROR);
+  res.json({
+    sucess: false,
+    code: err.code || HttpStatusCodes.INTERNAL_SERVER_ERROR,
+    message:
+      err.message ||
+      HttpStatusCodes.getStatusText(HttpStatusCodes.INTERNAL_SERVER_ERROR),
+  });
 };
