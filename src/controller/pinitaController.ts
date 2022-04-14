ed752891bc796cc4ba18325003aa52ec9058a09b
devbyul <kino@ianswer.co.kr>
2022-04-14 10:23:55 +0900

    feat: [ pinataController ] mainImg, thumnailImg pinata Upload using pinataService[pinFileToIPFS]

--- src/controller/pinitaController.ts
+++ src/controller/pinitaController.ts
@@ -28,5 +28,32 @@ export default class PinataController extends baseController {
   static uploadFilePage: IController = async (req: Request, res: Response) => {
     const { description, name, supply }: INFTMetadataDTO = req.body;
     const fileInfos = JSON.parse(JSON.stringify(req.files));
+
+    try {
+      // mainImg PinataUpload
+      let mainResult = await PinataService.imgUploadPinata(
+        fileInfos.mainImg[0].path
+      );
+
+      if (Object.keys(mainResult).length > 3) {
+        throw "is Duplicate ImageFile";
+      }
+      let originalName = fileInfos.mainImg[0].filename;
+      let mainIpfsHash = mainResult.IpfsHash;
+
+      // thumbnailImg PinataUpload
+      let thumbReult = await PinataService.imgUploadPinata(
+        fileInfos.thumbnailImg[0].path
+      );
+      let thumbName = fileInfos.thumbnailImg[0].filename;
+      let thumbIpfsHash = thumbReult.IpfsHash;
+    } catch (err: any) {
+      let error = {
+        status: 401,
+        code: 401,
+        message: err,
+      };
+      ApiResponse.error(res, error);
+    }
   };
 }
