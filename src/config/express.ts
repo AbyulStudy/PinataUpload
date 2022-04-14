168e93cdfc45b8d304f6ac8ec650a878bfb7ca52
devbyul <kino@ianswer.co.kr>
2022-04-14 10:08:28 +0900

    feat: express module using morgan && static path && errorrHanlder

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import indexRouter from "../route/index";
 import apiRouter from "../route/api";
-import { notFoundErrorHandler } from "../middleware/apiErrorHandler";
+import {
+  notFoundErrorHandler,
+  errorHandler,
+} from "../middleware/apiErrorHandler";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+
+app.use(express.static("public"));
+app.use("/uploadFiles", express.static("uploadFiles"));
 
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
 
 app.use(notFoundErrorHandler);
+app.use(errorHandler);
 export default app;
